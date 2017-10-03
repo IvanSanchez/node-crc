07939dd69a223ebbf0297761305361dc8f4a9283
Iván Sánchez Ortega <ivan@sanchezortega.es>
2017-10-03 15:22:42 +0200

    Refactor type casting into buffer/uint8array. Remove hard dependency on Buffer.

--- src/define_crc.js
+++ src/define_crc.js
@@ -1,5 +1,34 @@
+
+let castToBytes;
+
+if (typeof Uint8Array !== 'undefined' && Uint8Array.from &&
+    typeof TextDecoder !== 'undefined') {
+
+    const utf8Decoder = new TextDecoder('utf-8');
+
+    castToBytes = function (arr) {
+        if (arr instanceof Uint8Array) {
+            return arr;
+        } else if (typeof arr === 'string') {
+            return utf8Decoder(arr);
+        } else {
+            return Uint8Array.from(arr);
+        }
+    }
+} else {
+    const Buffer = require('buffer').Buffer;
+
+    castToBytes = Buffer.from && Buffer.alloc && Buffer.allocUnsafe && Buffer.allocUnsafeSlow
+        ? (arr) => Buffer.isBuffer(arr) ? arr : Buffer.from(arr)
+
+        // support for Node < 5.10
+        : (arr) => Buffer.isBuffer(arr) ? arr : new Buffer(arr)
+        ;
+}
+
+
 export default function (model, calc) {
-  const fn = (buf, previous) => calc(buf, previous) >>> 0;
+  const fn = (buf, previous) => calc(castToBytes(buf), previous) >>> 0;
   fn.signed = calc;
   fn.unsigned = fn;
   fn.model = model;
